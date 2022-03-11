3828b40b2cdec327e24ee4c927b39007f97ca194
Machhindra Thange <machhindra.thange@refinitiv.com>
2022-03-11 19:33:11 +0000

    Update entrypoint.sh

--- DOCKER/entrypoint.sh
+++ DOCKER/entrypoint.sh
@@ -1,72 +1,172 @@
-FROM alpine:3.10
+#!/usr/bin/env bash
 
-ENV DOCKER_CHANNEL=stable \
-    DOCKER_VERSION=19.03.2 \
-    DOCKER_COMPOSE_VERSION=1.24.1 \
-    DOCKER_SQUASH=0.2.0
+# Inspired by concourse/docker-image-resource:
+# https://github.com/concourse/docker-image-resource/blob/master/assets/common.sh
 
-# Install Docker, Docker Compose, Docker Squash
-RUN apk update
+set -o errexit -o pipefail -o nounset
 
-# Install base and dev packages
-RUN apk add --no-cache --virtual .build-deps
-RUN apk add bash
+# Waits DOCKERD_TIMEOUT seconds for startup (default: 60)
+DOCKERD_TIMEOUT="${DOCKERD_TIMEOUT:-60}"
+# Accepts optional DOCKER_OPTS (default: --data-root /scratch/docker)
+DOCKER_OPTS="${DOCKER_OPTS:-}"
 
-# Install build packages
-RUN apk add make && apk add curl && apk add openssh
+# Constants
+DOCKERD_PID_FILE="/tmp/docker.pid"
+DOCKERD_LOG_FILE="/tmp/docker.log"
 
-# Install git
-RUN apk add git
+sanitize_cgroups() {
+  local cgroup="/sys/fs/cgroup"
 
-# Install nodejs
-RUN apk add nodejs
+  mkdir -p "${cgroup}"
+  if ! mountpoint -q "${cgroup}"; then
+    if ! mount -t tmpfs -o uid=0,gid=0,mode=0755 cgroup "${cgroup}"; then
+      echo >&2 "Could not make a tmpfs mount. Did you use --privileged?"
+      exit 1
+    fi
+  fi
+  mount -o remount,rw "${cgroup}"
 
-# Install yarn
-RUN apk add yarn
+  # Skip AppArmor
+  # See: https://github.com/moby/moby/commit/de191e86321f7d3136ff42ff75826b8107399497
+  export container=docker
 
-# Set timezone to UTC by default
-RUN ln -sf /usr/share/zoneinfo/Etc/UTC /etc/localtime
+  # Mount /sys/kernel/security
+  if [[ -d /sys/kernel/security ]] && ! mountpoint -q /sys/kernel/security; then
+    if ! mount -t securityfs none /sys/kernel/security; then
+      echo >&2 "Could not mount /sys/kernel/security."
+      echo >&2 "AppArmor detection and --privileged mode might break."
+    fi
+  fi
 
-# Install aws-cli
-RUN apk -Uuv add groff less python py-pip
-RUN pip install awscli
-RUN apk --purge -v del py-pip
-RUN rm /var/cache/apk/*
-RUN apk -v --no-cache --update add \
-        musl-dev \
-        gcc \
-        python3 \
-        python3-dev
-RUN python3 -m ensurepip --upgrade \
-        && pip3 install --upgrade pip
-RUN pip3 install --upgrade awscli aws-sam-cli
-RUN pip3 uninstall --yes pip \
-        && apk del python3-dev gcc musl-dev
+  sed -e 1d /proc/cgroups | while read sys hierarchy num enabled; do
+    if [[ "${enabled}" != "1" ]]; then
+      # subsystem disabled; skip
+      continue
+    fi
 
-RUN apk --update --no-cache add \
-        bash \
-        curl \
-        device-mapper \
-        py-pip \
-        python-dev \
-        iptables \
-        util-linux \
-        ca-certificates \
-        gcc \
-        libc-dev \
-        libffi-dev \
-        openssl-dev \
-        make \
-        && \
-    apk upgrade && \
-    curl -fL "https://download.docker.com/linux/static/${DOCKER_CHANNEL}/x86_64/docker-${DOCKER_VERSION}.tgz" | tar zx && \
-    mv /docker/* /bin/ && chmod +x /bin/docker* && \
-    pip install docker-compose==${DOCKER_COMPOSE_VERSION} && \
-    curl -fL "https://github.com/jwilder/docker-squash/releases/download/v${DOCKER_SQUASH}/docker-squash-linux-amd64-v${DOCKER_SQUASH}.tar.gz" | tar zx && \
-    mv /docker-squash* /bin/ && chmod +x /bin/docker-squash* && \
-    rm -rf /var/cache/apk/* && \
-    rm -rf /root/.cache
+    grouping="$(cat /proc/self/cgroup | cut -d: -f2 | grep "\\<${sys}\\>")"
+    if [[ -z "${grouping}" ]]; then
+      # subsystem not mounted anywhere; mount it on its own
+      grouping="${sys}"
+    fi
 
-COPY entrypoint.sh /bin/entrypoint.sh
-CMD ["/bin/bash"]
-ENTRYPOINT ["entrypoint.sh"]
+    mountpoint="${cgroup}/${grouping}"
+
+    mkdir -p "${mountpoint}"
+
+    # clear out existing mount to make sure new one is read-write
+    if mountpoint -q "${mountpoint}"; then
+      umount "${mountpoint}"
+    fi
+
+    mount -n -t cgroup -o "${grouping}" cgroup "${mountpoint}"
+
+    if [[ "${grouping}" != "${sys}" ]]; then
+      if [[ -L "${cgroup}/${sys}" ]]; then
+        rm "${cgroup}/${sys}"
+      fi
+
+      ln -s "${mountpoint}" "${cgroup}/${sys}"
+    fi
+  done
+
+  # Initialize systemd cgroup if host isn't using systemd.
+  # Workaround for https://github.com/docker/for-linux/issues/219
+  if ! [[ -d /sys/fs/cgroup/systemd ]]; then
+    mkdir "${cgroup}/systemd"
+    mount -t cgroup -o none,name=systemd cgroup "${cgroup}/systemd"
+  fi
+}
+
+# Setup container environment and start docker daemon in the background.
+start_docker() {
+  echo >&2 "Setting up Docker environment..."
+  mkdir -p /var/log
+  mkdir -p /var/run
+
+  sanitize_cgroups
+
+  # check for /proc/sys being mounted readonly, as systemd does
+  if grep '/proc/sys\s\+\w\+\s\+ro,' /proc/mounts >/dev/null; then
+    mount -o remount,rw /proc/sys
+  fi
+
+  local docker_opts="${DOCKER_OPTS:-}"
+
+  # Pass through `--garden-mtu` from gardian container
+  if [[ "${docker_opts}" != *'--mtu'* ]]; then
+    local mtu="$(cat /sys/class/net/$(ip route get 8.8.8.8|awk '{ print $5 }')/mtu)"
+    docker_opts+=" --mtu ${mtu}"
+  fi
+
+  # Use Concourse's scratch volume to bypass the graph filesystem by default
+  if [[ "${docker_opts}" != *'--data-root'* ]] && [[ "${docker_opts}" != *'--graph'* ]]; then
+    docker_opts+=' --data-root /scratch/docker'
+  fi
+
+  rm -f "${DOCKERD_PID_FILE}"
+  touch "${DOCKERD_LOG_FILE}"
+
+  echo >&2 "Starting Docker..."
+  dockerd ${docker_opts} &>"${DOCKERD_LOG_FILE}" &
+  echo "$!" > "${DOCKERD_PID_FILE}"
+}
+
+# Wait for docker daemon to be healthy
+# Timeout after DOCKERD_TIMEOUT seconds
+await_docker() {
+  local timeout="${DOCKERD_TIMEOUT}"
+  echo >&2 "Waiting ${timeout} seconds for Docker to be available..."
+  local start=${SECONDS}
+  timeout=$(( timeout + start ))
+  until docker info &>/dev/null; do
+    if (( SECONDS >= timeout )); then
+      echo >&2 'Timed out trying to connect to docker daemon.'
+      if [[ -f "${DOCKERD_LOG_FILE}" ]]; then
+        echo >&2 '---DOCKERD LOGS---'
+        cat >&2 "${DOCKERD_LOG_FILE}"
+      fi
+      exit 1
+    fi
+    if [[ -f "${DOCKERD_PID_FILE}" ]] && ! kill -0 $(cat "${DOCKERD_PID_FILE}"); then
+      echo >&2 'Docker daemon failed to start.'
+      if [[ -f "${DOCKERD_LOG_FILE}" ]]; then
+        echo >&2 '---DOCKERD LOGS---'
+        cat >&2 "${DOCKERD_LOG_FILE}"
+      fi
+      exit 1
+    fi
+    sleep 1
+  done
+  local duration=$(( SECONDS - start ))
+  echo >&2 "Docker available after ${duration} seconds."
+}
+
+# Gracefully stop Docker daemon.
+stop_docker() {
+  if ! [[ -f "${DOCKERD_PID_FILE}" ]]; then
+    return 0
+  fi
+  local docker_pid="$(cat ${DOCKERD_PID_FILE})"
+  if [[ -z "${docker_pid}" ]]; then
+    return 0
+  fi
+  echo >&2 "Terminating Docker daemon."
+  kill -TERM ${docker_pid}
+  local start=${SECONDS}
+  echo >&2 "Waiting for Docker daemon to exit..."
+  wait ${docker_pid}
+  local duration=$(( SECONDS - start ))
+  echo >&2 "Docker exited after ${duration} seconds."
+}
+
+start_docker
+trap stop_docker EXIT
+await_docker
+
+# do not exec, because exec disables traps
+if [[ "$#" != "0" ]]; then
+  "$@"
+else
+  bash --login
+fi
